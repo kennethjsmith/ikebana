62f2e87e66acbd1e20f9d04ee82500c9521793f9
Ken Smith <42561470+kennethjsmith@users.noreply.github.com>
2022-01-28 21:37:00 -0800

    gunwork

--- gun.js
+++ gun.js
@@ -1,59 +1,38 @@
 class Gun {
     constructor(type, game) {
+        this.count = 0;
+
         this.type = type;
         this.game = game;
         this.game.gun = this;
 
         this.facing = "right"; // left or right
 
-        this.xMap = this.game.camera.startXPlayer;
-        this.yMap = this.game.camera.startYPlayer;
-        //barrel tip for bullet spawn
-        // this.xBarrelTipCanvas = this.xCanvas+55;
-        // this.yBarrelTipCanvas = this.yCanvas+30;
-        // this.xBarrelTipMap = this.xMap+55;
-        // this.yBarrelTipMap = this.yMap+30;
-
-
-        // if(this.facing == "right"){
-        //     this.xBarrelTip = this.xCanvas +50;
-        //     this.yBarrelCanvas = 100;
-        // }
-        // else{a
-        //     this.xBarrelCanvas = 400;
-        //     this.yBarrelCanvas = 300;
-        // }
-
+        this.mapOffset = 25;
+        this.additionalMapOffset = 40;
+    
+        console.log("" + this.game.camera.startXPlayer)
+        this.xMap = this.game.camera.startXPlayer + this.mapOffset;
+        this.yMap = this.game.camera.startYPlayer + this.mapOffset;
 
         this.rotation = 0;
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/guns.png");
-        this.scale = 3;
+        this.SIZE = 38; // num of pixels wide
+        this.SCALE = 3;
+        this.spriteSize = this.SIZE * this.SCALE;
+
+        this.sprites = new Map;
+        this.sprites.set("uzi", new Map);
+        this.sprites.get("uzi").set("left", new Map);
+        this.sprites.get("uzi").set("right", new Map);
+        this.sprites.set("laser", new Map);
+        this.sprites.get("laser").set("left", new Map);
+        this.sprites.get("laser").set("right", new Map);
 
         this.guncooldown = 20;
         this.uzicooldown = 5;                   
-        
-        
-        this.animations = new Map;
-        this.loadAnimations();
-
-        this.animation = this.animations.get("right");
-        //Animator constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration) {
 
     };
-
-    loadAnimations() {
-        if(this.type == "uzi"){
-            this.animations.set("left", new Animator(this.spritesheet, 0, 0, 38, 38, 1, 1));
-            this.animations.set("right", new Animator(this.spritesheet, 38, 0, 38, 38, 1, 1));
-        }
-        else if(this.type == "laser"){
-            this.animations.set("left", new Animator(this.spritesheet, 76, 0, 38, 38, 1, 1));
-            this.animations.set("right", new Animator(this.spritesheet, 114, 0, 38, 38, 1, 1));
-        }
-    };
-    // update() {
-    //     this.rotation = Math.atan2((this.game.mouse.x - this.x), -(this.game.mouse.y - this.y)); //Math.atan2(this.game. -x, -(this.game.mouse) );
-    // }
 
     update() {
 
@@ -65,10 +44,15 @@ class Gun {
     //     //     this.facing = "right";
     //     // }
 
-    //     this.rotation = Math.atan2((this.game.mouseX - this.xMap), -(this.game.mouseY - this.yMap)) - Math.PI/2;
+    console.log("PI: "+ Math.PI/4);
+    // console.log("mouse y: " + this.game.mouseY)
+    // console.log("mouse x: " + this.game.mouseX)
+    console.log("xmap: " + this.xMap);
+    console.log("yMap: " + this.yMap);
+    this.rotation = Math.atan2((this.game.crosshair.yMap - this.yMap), (this.game.crosshair.xMap - this.xMap)) + Math.PI/4;
     //     // add alpha angle to rotation to aim gun barrel directly at cursor
         
-        this.rotation -= Math.atan2(Math.hypot((this.game.mouseX - this.xMap),(this.game.mouseY - this.yMap)),10);
+        //this.rotation -= Math.atan2(Math.hypot((this.game.mouseX - this.xMap),(this.game.mouseY - this.yMap)),10);
 
     //     // update the animation
     //     //console.log(this.facing + this.rotation);
@@ -112,41 +96,57 @@ class Gun {
     };
 
     move(x,y){
-        this.xMap = x;
-        this.yMap = y;
+
+        this.xMap = x + this.mapOffset;
+        this.yMap = y + this.mapOffset;
     };
 
     draw(ctx) {
-    //     //ctx.rotate(this.rotation);
-    //     // if(this.facing == "right"){
-    //     //      ctx.rotate(this.rotation);
-    //     // }
-    //     // left facing gun needs additional pi rotation
-    //     // if(this.facing == "left"){
-    //     //     this.rotation -= Math.PI;
-    //     //     ctx.rotate(this.rotation);
-    //     //     this.rotation += Math.PI;
-    //     // }
-    //     //ctx.translate(this.xCanvas, this.yCanvas);
+        //ctx.rotate(this.rotation);
+        // if(this.facing == "right"){
+        //      ctx.rotate(this.rotation);
+        // }
+        // left facing gun needs additional pi rotation
+        // if(this.facing == "left"){
+        //     this.rotation -= Math.PI;
+        //     ctx.rotate(this.rotation);
+        //     this.rotation += Math.PI;
+        // }
+        //ctx.translate(this.xCanvas, this.yCanvas);
 
 
+        let offscreenCanvas = null;
+        let degrees = Math.floor(this.rotation * (180/Math.PI));
+        console.log("degrees: " + degrees + ", radians: " + this.rotation);
 
 
+        if (this.sprites.get("uzi").get("left").has(degrees)) {
+            offscreenCanvas = this.sprites.get("uzi").get("left").get(degrees);
+        } else {
+            this.count++;
+            // create the canvas with the rotated image
+            offscreenCanvas = document.createElement('canvas')                                                              
+            offscreenCanvas.width = (2*(this.spriteSize / 3)*2);
+            offscreenCanvas.height = (2*(this.spriteSize / 3)*2);
+            let offscreenCtx = offscreenCanvas.getContext('2d');
+            offscreenCtx.imageSmoothingEnabled = false;
+            offscreenCtx.save();
+            offscreenCtx.translate(2*(this.spriteSize / 3), this.spriteSize / 2);
+            offscreenCtx.rotate(this.rotation);
+            offscreenCtx.translate(-2*(this.spriteSize / 3), -this.spriteSize / 2);     
+                
+            offscreenCtx.drawImage(this.spritesheet, this.SIZE, 0, this.SIZE, this.SIZE, 10, 10,this.spriteSize,this.spriteSize)
+            offscreenCtx.restore();
+            this.sprites.get("uzi").get("left").set(degrees, offscreenCanvas);
+        }
+        console.log(this.count);
 
-        let offscreenCanvas = document.createElement('canvas')                                                              
-        let dimension = 38 * this.scale;
-        offscreenCanvas.width = dimension;
-        offscreenCanvas.height = dimension;
-        let offscreenCtx = offscreenCanvas.getContext('2d');
-        offscreenCtx.imageSmoothingEnabled = true;
-        offscreenCtx.save();
-        offscreenCtx.translate(offscreenCanvas.width / 2, offscreenCanvas.height / 2);
-        offscreenCtx.rotate(this.rotation);
-        offscreenCtx.translate(-offscreenCanvas.width / 2, -offscreenCanvas.height / 2);
-
+        ctx.drawImage(offscreenCanvas, this.xMap-this.game.camera.x, this.yMap-this.game.camera.y);
+        //this.animation.drawFrame(this.game.clockTick, ctx, this.xMap-this.game.camera.x, this.yMap-this.game.camera.y, this.SCALE);
         
-        this.animation.drawFrame(this.game.clockTick, ctx, this.xMap-this.game.camera.x, this.yMap-this.game.camera.y, this.scale);
-        offscreenCtx.restore();
+
+
+
         // this.bullets.forEach(bullet => {
         //     bullet.draw(ctx);
         // });
