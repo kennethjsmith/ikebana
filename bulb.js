bd3e0e002bc703dcb199c7abb9ffa6d2a9725fff
Ken Smith <42561470+kennethjsmith@users.noreply.github.com>
2022-02-26 19:21:56 -0800

    new ooze shadow, bb on bulb

--- bulb.js
+++ bulb.js
@@ -5,22 +5,27 @@ class Bulb {
         else if (this.game.camera.level == "level2") this.spritesheet = ASSET_MANAGER.getAsset("./sprites/bulb2.png");
 
         this.scale = 5;
-        this.spriteHeight = 20 * this.scale; // scaled height
-        this.spriteWidth = 12 * this.scale; // scaled width
+        this.spriteHeight = 8 * this.scale; // scaled height
+        this.spriteWidth = 6 * this.scale; // scaled width
 
         this.xMap = x;
         this.yMap = y;
 
         this.animations = new Map;
         this.loadAnimations();
+        this.updateBoundingBox();
 
         this.sparkle = false;
         this.animation = this.animations.get(this.sparkle);
     };
 
     loadAnimations() {
-        this.animations.set(false, new Animator(this.spritesheet, 0, 0, 10, 10, 1, 1));
-        this.animations.set(true, new Animator(this.spritesheet, 0, 0, 10, 10, 8, .08));
+        this.animations.set(false, new Animator(this.spritesheet, 0, 0, 10, 12, 1, 1));
+        this.animations.set(true, new Animator(this.spritesheet, 0, 0, 10, 12, 8, .08));
+    };
+
+    updateBoundingBox() {
+        this.boundingBox = new BoundingBox(this.xMap, this.yMap + 2*(this.spriteHeight/3), this.spriteWidth, this.spriteHeight/3);
     };
 
     update() {
